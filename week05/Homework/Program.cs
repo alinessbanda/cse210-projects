2b10b3c9c9c3151194297bccb17c0c70b456685f
alinessbanda <abanda@byupathway.edu>
2025-06-08 23:49:12 -0600

    Completed Week05/Homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment myAssignment = new Assignment("Aliness Banda", "Science - Plant Life");
+
+        string summary = myAssignment.GetSummary();
+        Console.WriteLine(summary);
+
+        MathAssignment math = new MathAssignment("Aliness Banda", "Fractions", "7.3", "20-21");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        WritingAssignment writing = new WritingAssignment("Aliness Banda", "African History", "The Impact of colonialism in Zambia");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
+
     }
 }
