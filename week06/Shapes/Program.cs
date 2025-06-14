406b5d1f3522d0ff94fa8758a7ff14fdcc3e468f
alinessbanda <abanda@byupathway.edu>
2025-06-14 13:53:39 -0600

    Completed week06/Shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Pink", 3);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Orange", 4, 5);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Turquoise", 6);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
     }
 }
